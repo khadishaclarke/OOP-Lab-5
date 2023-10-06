46a1ec440c4d82f9682f97fa995f50257c738d22
Khadisha <123189196+khadishaclarke@users.noreply.github.com>
2023-10-06 17:37:06 +0000

    code review

--- Lab files - Week 5-20231006/Refactoring/GildedRose.java
+++ Lab files - Week 5-20231006/Refactoring/GildedRose.java
@@ -8,6 +8,7 @@ class GildedRose {
         this.items = items;
     }
 
+    //pull out logic that can be in its own function to reduce repetition[eg. -1 quality func might be called lowerQuality vice versa]
     public void updateQuality() {
         for (int i = 0; i < items.length; i++) {
             if (!items[i].name.equals("Aged Brie") && !items[i].name.equals("Backstage passes to a TAFKAL80ETC concert")) {
