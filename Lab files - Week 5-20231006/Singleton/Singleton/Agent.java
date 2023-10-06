eba683489689fd797a7b8ace9d926e45b7431bec
Khadisha <123189196+khadishaclarke@users.noreply.github.com>
2023-10-06 12:22:27 -0400

    Add files via upload

---
+++ Lab files - Week 5-20231006/Singleton/Singleton/Agent.java
@@ -0,0 +1,23 @@
+
+/**
+ *
+ * @author phaedramohammed
+ * Question 1: fix the code n agent that keeps track of the number of 
+ * requests made for a reference to the agent using the Singleton design pattern.
+ * 
+ */
+
+public class Agent {
+    private int numRequests;
+    
+    private Agent(){
+        numRequests = 0;
+    }
+    public static Agent getAgent(){
+        numRequests++;
+        return this;
+    }
+    public String toString(){
+        return "C'est moi, le singleton agent pour le " +numRequests + " fois!";
+    }
+}
